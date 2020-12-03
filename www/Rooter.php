de322fe7c4ac40672104b737bd4439a27a0e14a1
Kélian <kelian.raoult_severac@edu.devinci.fr>
2020-12-03 13:41:05 +0100

    Update project structure with mvc

---
+++ www/Rooter.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Controller\ConnexionController;
+
+
+if (array_key_exists("page", $_GET)) {
+
+    switch ($_GET["page"]) {
+        case 'connexion':
+            $controller = new ConnexionController();
+            $controller->connexion();
+            break;
+        
+        
+    }
+} else {
+    $controller = new SignUpController();
+}
